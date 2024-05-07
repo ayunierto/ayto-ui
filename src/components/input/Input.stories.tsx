e15159a6597e653ec210ec24eac5231f7b54980a
Alcides Turruellas <ayunierto@gmail.com>
2024-05-07 03:49:48 -0500

    updated input component

--- src/components/input/Input.stories.tsx
+++ src/components/input/Input.stories.tsx
@@ -1,11 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import Input from "./Input";
+import Icon from "../icon/Icon";
 
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Input> = {
   title: "Components/Input",
-
+  argTypes: {
+    className: { control: { type: "text" } },
+    style: { control: { type: "object" } },
+    type: { control: { type: "inline-radio" } },
+    placeholder: { control: { type: "text" } },
+    value: { type: "string" },
+    onChange: { type: "function" },
+    contentAfter: { control: { type: "text" } },
+    contentBefore: { control: { type: "text" } },
+    id: { control: { type: "text" } },
+    size: { control: { type: "inline-radio" } },
+  },
   tags: ["autodocs"],
   component: Input,
 };
@@ -25,4 +37,53 @@ export const WithPlaceholder: Story = {
     // The args you need here will depend on your component
     placeholder: "I'm placeholder",
   },
+};
+
+export const Content: Story = {
+  args: {
+    // The args you need here will depend on your component
+  },
+  render: (args) => (
+    <div
+      style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+      {...args}
+    >
+      <div>
+        <label htmlFor="searh">Searh</label>
+        <Input contentAfter={<Icon clickable icon="search" />} id="searh" />
+      </div>
+      <div>
+        <label htmlFor="amount">Amount</label>
+        <Input contentBefore="$" contentAfter=".00" id="amount" />
+      </div>
+      <div>
+        <label htmlFor="person">Amount</label>
+        <Input contentBefore={<Icon icon="person" />} id="person" />
+      </div>
+    </div>
+  ),
+};
+
+export const Size: Story = {
+  args: {
+    // The args you need here will depend on your component
+  },
+  render: (args) => (
+    <div
+      style={{ display: "flex", flexDirection: "column", gap: "10px" }}
+      {...args}
+    >
+      <Input
+        size="small"
+        contentAfter={<Icon size="small" clickable icon="search" />}
+        id="searh"
+      />
+      <Input size="medium" contentBefore="$" contentAfter=".00" id="amount" />
+      <Input
+        size="large"
+        contentBefore={<Icon size="large" icon="person" />}
+        id="person"
+      />
+    </div>
+  ),
 };
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,37 +1,53 @@
+import React from "react";
 import "./Input.css";
 
 type Props = {
-  children?: React.ReactNode;
+  /**
+   * Additional classes to be added to the Button
+   */
   className?: string;
+  /**
+   * Additional styles to be added to the Button
+   */
   style?: React.CSSProperties;
   type?: React.HTMLInputTypeAttribute;
   placeholder?: string;
   value?: string;
   onChange?: () => void;
+  contentBefore?: React.ReactNode;
+  contentAfter?: React.ReactNode;
+  id?: string;
+  size?: "small" | "medium" | "large";
 };
 
 const Input = ({
-  children,
-  className,
+  className = "",
   style,
   type = "text",
   placeholder,
   value,
   onChange,
+  contentAfter,
+  contentBefore,
+  id,
+  size = "medium",
   ...props
 }: Props) => {
   return (
-    <input
-      type={type}
-      className={`${className}`}
-      style={style}
-      {...props}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    >
-      {children}
-    </input>
+    <div className="input">
+      {contentBefore}
+      <input
+        id={id}
+        type={type}
+        className={`${size} ${className}`}
+        style={style}
+        {...props}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {contentAfter}
+    </div>
   );
 };
 
